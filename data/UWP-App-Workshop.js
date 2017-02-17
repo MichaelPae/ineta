5b3489ad01d6f7b49e362cbf936584d6d8b76347
MichaelPae <paetzold@t-online.de>
2017-02-17 23:02:08 +0100

    aufgeräumt

---
+++ data/UWP-App-Workshop.js
@@ -0,0 +1,12 @@
+   { "TreffenNr": 203,
+     "Session":
+     [  { "SessionId": "20301",
+      "Caption": "Strukturierung eines UWP-App-Workshops",
+      "Summary": "Gemeinsames Brainstorming wie wir eine Schritt-für-Schritt-Entwicklung einer DevGroup UWP-App (Universelle Windows-Plattform) unter Einbindung von PRISM 6 strukturieren könnten.",
+      "Items": [ "Item 1", "Item 2", "Item 3", "Item 4" ],
+      "Link": "",
+      "Speaker": "Jedermann",
+      "Leute": 0,
+      "Firmen": 0 }
+     ]
+   },
